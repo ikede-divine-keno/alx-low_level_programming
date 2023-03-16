9ab33f892c7dc54c4c267f36bd1a2073bf2a761d
ikede-divine-keno <ikededivine@gmail.com>
2023-03-16 09:02:30 -0700

    4-puts.c modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
-int main()
+
+/**
+ * main - Prints \"Programming is like building a multilingual puzzle followed
+ * by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
 	char alx[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(alx);
 
-	return 0;
+	return (0);
 }
