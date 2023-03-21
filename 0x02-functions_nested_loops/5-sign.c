17a02d5c66d16ebc5665b5c3023b6e4d2e608628
ikede-divine-keno <ikededivine@gmail.com>
2023-03-21 12:56:14 -0700

    5-sign.c added

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number.
+ * @n: An integer number.
+ *
+ * Return: 1 if positive, 0 if 0 and -1 if negative.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
