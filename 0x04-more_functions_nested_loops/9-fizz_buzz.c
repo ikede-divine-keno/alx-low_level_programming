2853a766654a68357a482694fa9a7556311d35ac
ikede-divine-keno <ikededivine@gmail.com>
2023-03-23 18:21:20 -0700

    9-fizz_buzz modified again

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,13 +16,25 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 15) == 0)
+		{
 			printf("FizzBuzz ");
+		}
 		else if ((n % 5) == 0)
+		{
 			printf("Buzz ");
+		}
+		else if (n == 100)
+		{
+			printf("Buzz");
+		}
 		else if ((n % 3) == 0)
+		{
 			printf("Fizz ");
+		}
 		else
+		{
 			printf("%d ", n);
+		}
 	}
 	printf("\n");
 
