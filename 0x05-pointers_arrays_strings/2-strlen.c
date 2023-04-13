5c569fd2bd57dd0cf036c1edeee4b32aeb9639de
ikede-divine-keno <ikededivine@gmail.com>
2023-04-13 11:18:36 -0700

    2-strlen.c added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: input string
+ * Return:  length of string
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+
+	}
+	return (a);
+}
