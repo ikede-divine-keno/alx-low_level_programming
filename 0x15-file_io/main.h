18338ded1b0f84e2326926841fc251b4aefc3081
ikede-divine-keno <ikededivine@gmail.com>
2023-05-08 21:34:24 -0700

    100-elf_header.c modified

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,20 +7,20 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <errno.h>
-#include <stdbool.h>
 #include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void read_elf_header(int32_t fd, Elf32_Ehdr *elf_header);
-bool is_ELF(Elf32_Ehdr eh);
-void print_os(Elf32_Ehdr eh);
-void print_typ(Elf32_Ehdr eh);
-void print_clas(Elf32_Ehdr eh);
-void print_dta(Elf32_Ehdr eh);
-void print_vrsn(Elf32_Ehdr eh);
-void print_elf_header(Elf32_Ehdr elf_header);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif
