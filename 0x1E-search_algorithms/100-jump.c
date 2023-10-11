8de8e2aa9c2fc99a20f9039b689f00378446c2b4
ikede-divine-keno <ikededivine@gmail.com>
2023-10-11 10:37:03 -0700

    100-jump, 100-jump.c and 100-main.c added

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,72 @@
+#include "search_algos.h"
+/**
+ * _sqrt - calculate the square root of a number
+ * @x: number
+ * Return: guess
+ */
+double _sqrt(double x)
+{
+	double guess = x / 2.0; /*Initial guess, can be any positive value*/
+	double prev_guess = 0.0; /*Previous guess*/
+	const double epsilon = 0.000001;/* A small value to control the precision*/
+
+	if (x < 0)
+	{
+		return (-1.0);
+	}
+
+	while (fabs(guess - prev_guess) > epsilon)
+	{
+		prev_guess = guess;
+		guess = 0.5 * (guess + x / guess); /*Newton-Raphson formula*/
+	}
+
+	return (guess);
+}
+/**
+ * jump_search - split an array into blocks and search between those blocks
+ * @array: Arrray
+ * @size: size of the array
+ * @value: value to search from
+ * Return: value or -1
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t step;
+	size_t prev = 0;
+	size_t i = 0;
+	size_t j;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	step = sqrt(size);
+	/*while (array[prev] < value && prev < size)*/
+	/*{*/
+	/*printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);*/
+	/*	if (array[prev] == value)*/
+	/*return (prev);*/
+	/*i = prev;*/
+
+	/*prev += step;}*/
+	for (i = prev = 0; prev < size && array[prev] < value;)
+	{
+		printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
+		i = prev;
+		prev += step;
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n", i, prev);
+
+	for (j = i; j < size && array[j] <= value; j++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", j, array[j]);
+
+		if (array[j] == value)
+		{
+			return ((int)j);
+		}
+	}
+
+	return (-1);
+}
